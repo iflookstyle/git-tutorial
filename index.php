35633d31a6ec87600096b9d65f8d8b235da08859
Flook <maikoitem@gmail.com>
2024-07-07 20:35:28 +0700

    Change Type

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+echo "i love you"; 
+echo "<br>"
+echo "my name is flook"
+echo "<br>"
+echo "Mahanakorn"
+echo "<br>"
+echo "my name is flook"
+echo "<br>"
+echo "i love takeshi"
+echo "<br>"
+?>
